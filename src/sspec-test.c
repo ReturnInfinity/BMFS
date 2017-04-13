74a403fb1eefa577e1842d13fa3de4aa58917c4c
Taylor Holberton <taylorcholberton@gmail.com>
2017-04-13 16:23:04 -0700

    corrected values of base 10 prefixes

--- src/sspec-test.c
+++ src/sspec-test.c
@@ -11,13 +11,13 @@ static int test_string(const char *str, uint64_t value);
 int main(void)
 {
 	test("4TiB", 4ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL);
-	test("4TB",  4ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL);
+	test("4TB",  4ULL * 1000ULL * 1000ULL * 1000ULL * 1000ULL);
 	test("4GiB", 4ULL * 1024ULL * 1024ULL * 1024ULL);
-	test("4GB",  4ULL * 1024ULL * 1024ULL * 1024ULL);
+	test("4GB",  4ULL * 1000ULL * 1000ULL * 1000ULL);
 	test("3MiB", 3ULL * 1024ULL * 1024ULL);
-	test("3MB",  3ULL * 1024ULL * 1024ULL);
+	test("3MB",  3ULL * 1000ULL * 1000ULL);
 	test("2KiB", 2ULL * 1024ULL);
-	test("2KB",  2ULL * 1024ULL);
+	test("2KB",  2ULL * 1000ULL);
 	test("1B", 1ULL);
 	test("1",  1ULL);
 	return EXIT_SUCCESS;
