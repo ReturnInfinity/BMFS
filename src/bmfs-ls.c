3ec431113a92dbf782b0111a0fd1e9d49fa1fbc7
Taylor Holberton <taylorcholberton@gmail.com>
2017-04-13 21:15:32 -0700

    added bmfs-ls.c

---
+++ src/bmfs-ls.c
@@ -0,0 +1,120 @@
+#include "bmfs.h"
+
+#include <errno.h>
+#include <getopt.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void help(const char *argv0)
+{
+	printf("usage: %s [options]\n", argv0);
+	printf("\n");
+	printf("options:\n");
+	printf("  --disk,      -d : specify disk image to use\n");
+	printf("  --help,      -h : display this help message\n");
+	printf("  --long-list, -l : print using long list format\n");
+	printf("  --version,   -v : display version information\n");
+	printf("\n");
+	printf("environment variables:\n");
+	printf("    BMFS_DISK : the disk image to use\n");
+}
+
+static void version(void)
+{
+	printf("%s\n", BMFS_VERSION_STRING);
+}
+
+int main(int argc, char **argv)
+{
+	struct option opts[] =
+	{
+		{ "disk", required_argument, NULL, 'd' },
+		{ "help", no_argument, NULL, 'h' },
+		{ "version", no_argument, NULL, 'v' },
+		{ 0, 0, 0, 0 }
+	};
+
+	const char *diskname = NULL;
+
+	while (1)
+	{
+		int c = getopt_long(argc, argv, "d:n:r:hv", opts, NULL);
+		if (c == 'd')
+			diskname = optarg;
+		else if (c == 'h')
+		{
+			help(argv[0]);
+			return EXIT_FAILURE;
+		}
+		else if (c == 'l')
+		{
+			use_long_list = 1;
+		}
+		else if (c == 'v')
+		{
+			version();
+			return EXIT_FAILURE;
+		}
+		else if (c == -1)
+			/* end of options */
+			break;
+		else if (c == ':')
+			/* invalid option */
+			return EXIT_FAILURE;
+		else if (c == '?')
+			/* missing option argument */
+			return EXIT_FAILURE;
+	}
+
+	if (diskname == NULL)
+	{
+		diskname = getenv("BMFS_DISK");
+		if (diskname == NULL)
+			diskname = "disk.image";
+	}
+
+	FILE *diskfile;
+	diskfile = fopen(diskname, "r+b");
+	if (diskfile == NULL)
+	{
+		fprintf(stderr, "%s: failed to open '%s': %s\n", argv[0], diskname, strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	struct BMFSDisk disk;
+	int err = bmfs_disk_init_file(&disk, diskfile);
+	if (err != 0)
+	{
+		fprintf(stderr, "%s: failed to initialize disk structure: %s\n", argv[0], strerror(-err));
+		fclose(diskfile);
+		return EXIT_FAILURE;
+	}
+
+	struct BMFSDir dir;
+	err = bmfs_disk_read_dir(&disk, &dir);
+	if (err != 0)
+	{
+		fprintf(stderr, "%s: failed to read root directory: %s\n", argv[0], strerror(-err));
+		fclose(diskfile);
+		return EXIT_FAILURE;
+	}
+
+	for (uint64_t i = 0ULL; i < 64ULL; i++)
+	{
+		const struct BMFSEntry *entry;
+		entry = &dir.Entries[i];
+		if (bmfs_entry_is_empty(entry))
+			continue;
+		else if (bmfs_entry_is_terminator(entry))
+			break;
+	
+		printf("%s\n", entry->FileName);
+	}
+
+	fclose(diskfile);
+
+	return EXIT_SUCCESS;
+}
+
