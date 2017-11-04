e676a26a4f6fe145dfb274a9a9616b6e708266fd
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-04 10:42:22 -0400

    building libbmfs.a

--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+set -e
+
 CC=gcc
 CFLAGS="${CFLAGS} -Wall -Wextra -Werror -Wfatal-errors -std=gnu99"
 CFLAGS="${CFLAGS} -fno-stack-protector -fomit-frame-pointer -mno-red-zone"
 CFLAGS="${CFLAGS} -I../include"
+
+AR=ar
+ARFLAGS=rcs
 
 LD=gcc
 LDFLAGS=-lc
@@ -11,6 +16,10 @@ LDFLAGS=-lc
 $CC $CFLAGS -c entry.c -o entry.o
 $CC $CFLAGS -c dir.c -o dir.o
 $CC $CFLAGS -c disk.c -o disk.o
-$CC $CFLAGS -c bmfs.c -o bmfs.o
+
+$AR $ARFLAGS libbmfs.a entry.o dir.o disk.o
+
 $CC $CFLAGS -c stdlib.c -o stdlib.o
-$LD $LDFLAGS entry.o dir.o disk.o bmfs.o stdlib.o -o bmfs
+$CC $CFLAGS -c bmfs.c -o bmfs.o
+
+$LD $LDFLAGS bmfs.o stdlib.o libbmfs.a -o bmfs
