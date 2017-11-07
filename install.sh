00d35bea952308bf5d8e4965df2d2c7ecf8aaa3b
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 20:17:33 -0500

    installing bmfs program too

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 set -e
 set -u
 
-mkdir -p $PREFIX/include
+mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
+mkdir -p $PREFIX/include
 
+cp src/bmfs $PREFIX/bin
 cp src/libbmfs.a $PREFIX/lib/
-
 cp -R include/bmfs $PREFIX/include/
