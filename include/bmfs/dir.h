96a15db37bbbf9fac3ed3e80b575e315a756c9fc
Taylor Holberton <taylorcholberton@gmail.com>
2018-03-24 18:43:27 -0400

    added bmfs_dir_set_disk() and bmfs_dir_import()

--- include/bmfs/dir.h
+++ include/bmfs/dir.h
@@ -29,6 +29,10 @@ struct BMFSDir
 	/** The entry containing the position and
 	 * other information regarding the directory. */
 	struct BMFSEntry Entry;
+	/** The offset to the directory entry. This
+	 * is used if an entry is added to this directory,
+	 * and the directory size must be updated. */
+	int64_t EntryOffset;
 	/** The entry that was last read from the
 	 * client. */
 	struct BMFSEntry CurrentEntry;
@@ -38,11 +42,37 @@ struct BMFSDir
 };
 
 /** Initializes a directory.
+ * After calling this function, also
+ * set the disk by calling @ref bmfs_dir_set_disk.
  * @param dir The directory to initialize.
  * @ingroup dir-api
  * */
 
 void bmfs_dir_init(struct BMFSDir *dir);
+
+/** Assigns the disk that will be used
+ * for the directory operations.
+ * @param dir An initialized directory structure.
+ * @param disk The disk to assign the directory.
+ * @ingroup dir-api
+ * */
+
+void bmfs_dir_set_disk(struct BMFSDir *dir,
+                       struct BMFSDisk *disk);
+
+/** Imports the directory from the disk.
+ * The disk must be currently pointing to
+ * the directory entry. If the entry is not
+ * a directory, or the read operation fails,
+ * then this function will fail. The directory
+ * should already have a directory assigned to
+ * it (if not, use @ref bmfs_dir_set_disk.)
+ * @param dir An initialized directory structure.
+ * @returns Zero on success, an error code on failure.
+ * @ingroup dir-api
+ * */
+
+int bmfs_dir_import(struct BMFSDir *dir);
 
 /** Moves to the next entry in the directory.
  * @param dir An initialized directory.
