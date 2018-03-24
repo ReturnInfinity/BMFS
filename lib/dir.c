cd415658473ada0058f0a8220ecd0f5384004739
Taylor Holberton <taylorcholberton@gmail.com>
2018-03-24 18:45:02 -0400

    implemented new directory functions

--- lib/dir.c
+++ lib/dir.c
@@ -1,14 +1,79 @@
-/* BareMetal File System Utility */
-/* Written by Ian Seyler of Return Infinity */
-/* v1.3.0 (2017 10 11) */
+/* ===============================================================
+ * Baremetal File System - A file system designed for BareMetal OS
+ * Copyright (C) 2008 - 2018 Return Infinity
+ * See COPYING for license information.
+ * ===============================================================
+ */
 
 #include <bmfs/dir.h>
+
+#include <bmfs/disk.h>
+
 #include <errno.h>
 
 void bmfs_dir_init(struct BMFSDir *dir)
 {
 	dir->Disk = NULL;
+	dir->EntryOffset = 0;
+	dir->CurrentIndex = 0;
 	bmfs_entry_init(&dir->Entry);
 	bmfs_entry_init(&dir->CurrentEntry);
+}
+
+void bmfs_dir_set_disk(struct BMFSDir *dir,
+                       struct BMFSDisk *disk)
+{
+	if ((dir != NULL) && (disk != NULL))
+	{
+		dir->Disk = disk;
+	}
+}
+
+int bmfs_dir_import(struct BMFSDir *dir)
+{
+	int64_t entry_offset = 0;
+
+	int err = bmfs_disk_tell(dir->Disk, &entry_offset);
+	if (err != 0)
+		return err;
+
+	struct BMFSEntry entry;
+
+	bmfs_entry_init(&entry);
+
+	err = bmfs_entry_read(&entry, dir->Disk);
+	if (err != 0)
+		return err;
+
+	dir->Entry = entry;
+	dir->EntryOffset = entry_offset;
 	dir->CurrentIndex = 0;
+
+	return 0;
+}
+
+const struct BMFSEntry *bmfs_dir_next(struct BMFSDir *dir)
+{
+	if (dir->CurrentIndex >= (BMFS_BLOCK_SIZE / BMFS_ENTRY_SIZE))
+		return NULL;
+
+	int64_t offset = 0;
+	offset += (int64_t) dir->CurrentIndex;
+	offset *= BMFS_ENTRY_SIZE;
+	offset += (int64_t) dir->Entry.Offset;
+
+	int err = bmfs_disk_seek(dir->Disk, offset, BMFS_SEEK_SET);
+	if (err != 0)
+		return NULL;
+
+	err = bmfs_entry_read(&dir->CurrentEntry, dir->Disk);
+	if (err != 0)
+		return NULL;
+
+	if (dir->CurrentEntry.Name[0] == 0)
+		return NULL;
+
+	dir->CurrentIndex++;
+
+	return &dir->CurrentEntry;
 }
